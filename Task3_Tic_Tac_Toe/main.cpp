aeee08834150b773bc99277c233abb443baf08c2
25ee017-stack <25ee017@rmkec.ac.in>
2026-01-07 16:20:58 +0530

    Task 3-Tic Tac Toe

---
+++ Task3_Tic_Tac_Toe/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3] = {
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'}
+};
+
+char currentPlayer = 'X';
+
+// Function to display the board
+void displayBoard() {
+    cout << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Function to check win
+bool checkWin() {
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == currentPlayer &&
+            board[i][1] == currentPlayer &&
+            board[i][2] == currentPlayer)
+            return true;
+
+        if (board[0][i] == currentPlayer &&
+            board[1][i] == currentPlayer &&
+            board[2][i] == currentPlayer)
+            return true;
+    }
+
+    if (board[0][0] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][2] == currentPlayer)
+        return true;
+
+    if (board[0][2] == currentPlayer &&
+        board[1][1] == currentPlayer &&
+        board[2][0] == currentPlayer)
+        return true;
+
+    return false;
+}
+
+// Function to switch player
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+int main() {
+    int move;
+    bool gameWon = false;
+
+    cout << "TIC TAC TOE GAME" << endl;
+
+    for (int turn = 0; turn < 9; turn++) {
+        displayBoard();
+
+        cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+        cin >> move;
+
+        int row = (move - 1) / 3;
+        int col = (move - 1) % 3;
+
+        if (move < 1 || move > 9 || board[row][col] == 'X' || board[row][col] =
